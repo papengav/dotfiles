8042d1047660f76fa581f8f993b7b0e874e5e7e1
Gavin Papenthien <papengav@gmail.com>
2025-06-08 02:55:27 -0500

    vim horizontal movements remaps

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -10,7 +10,10 @@ map("i", "jk", "<ESC>")
 -- My Keybinds
 map("n", "ZZ", "<cmd> qa <CR>", { desc = "General quit nvim" })
 map({ "n", "i" }, "<C-z>", ":undo <CR>", { desc = "General undo" })
-map({ "n", "i" }, "<C-s-z>", ":redo <CR>", { desc ="General redo" })
-
+map({ "n", "i" }, "<C-s-z>", ":redo <CR>", { desc = "General redo" })
+map({ "n" }, "<C-d>", "<C-d>zz", { desc = "Half page-down with vertically centered cursor" })
+map({ "n" }, "<C-u>", "<C-u>zz", { desc = "Half page-up with vertically centered cursor" })
+map({ "n" }, "n", "nzzzv", { desc = "Next with vertically centered cursor and auto unfold" })
+map({ "n" }, "N", "Nzzzv", { desc = "Previous with vertically centered cursor and auto unfold" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
